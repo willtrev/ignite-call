1594073a0f7f9e63765204cb92689b5f2a7520b0
Willian Trevisan <williantrev@gmail.com>
2024-08-11 01:21:33 -0400

    Configura Eslint e pageExtensions

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return <Heading as='h1'>Hello World</Heading>
-}
